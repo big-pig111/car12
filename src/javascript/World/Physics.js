cbdc4f9e41a343e7200073f189845a76966dd399
big-pig111 <xuhang123qwe@gmail.com>
2025-07-24 01:15:53 +0800

    Update Physics.js

--- src/javascript/World/Physics.js
+++ src/javascript/World/Physics.js
@@ -142,10 +142,10 @@ export default class Physics
         this.car.options.controlsSteeringSpeed = 0.005
         this.car.options.controlsSteeringMax = Math.PI * 0.17
         this.car.options.controlsSteeringQuad = false
-        this.car.options.controlsAcceleratinMaxSpeed = 0.055
-        this.car.options.controlsAcceleratinMaxSpeedBoost = 0.11
-        this.car.options.controlsAcceleratingSpeed = 2
-        this.car.options.controlsAcceleratingSpeedBoost = 3.5
+        this.car.options.controlsAcceleratinMaxSpeed = 0.025
+        this.car.options.controlsAcceleratinMaxSpeedBoost = 0.1
+        this.car.options.controlsAcceleratingSpeed = 1
+        this.car.options.controlsAcceleratingSpeedBoost = 3
         this.car.options.controlsAcceleratingQuad = true
         this.car.options.controlsBrakeStrength = 0.45
 
