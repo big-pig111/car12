a6545b0922e5943e0ee807a0e5e84b9a026c60a1
big-pig111 <xuhang123qwe@gmail.com>
2025-07-24 01:48:56 +0800

    Update InformationSection.js

--- src/javascript/World/Sections/InformationSection.js
+++ src/javascript/World/Sections/InformationSection.js
@@ -89,19 +89,19 @@ export default class InformationSection
         // Options
         this.links.options = [
             {
-                href: 'https://twitter.com/bruno_simon/',
+                href: 'https://x.com/halloffame_bonk',
                 labelTexture: this.resources.items.informationContactTwitterLabelTexture
             },
             {
-                href: 'https://github.com/brunosimon/',
+                href: 'https://x.com/halloffame_bonk',
                 labelTexture: this.resources.items.informationContactGithubLabelTexture
             },
             {
-                href: 'https://www.linkedin.com/in/simonbruno77/',
+                href: 'https://x.com/halloffame_bonk',
                 labelTexture: this.resources.items.informationContactLinkedinLabelTexture
             },
             {
-                href: 'mailto:simon.bruno.77@gmail.com',
+                href: 'https://x.com/halloffame_bonk',
                 labelTexture: this.resources.items.informationContactMailLabelTexture
             }
         ]
